8a4691ee252409adbd856cdce3f5bd4f865c8def
ZymanteL <zymantelelesiute@gmail.com>
2023-07-18 14:48:13 +0300

    test

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MovieDetails from './components/movieDetails/MovieDetails';
 
 function App() {
   return (
     <div>
-
+      <MovieDetails/>
     </div>
   );
 }
--- movie-app/src/components/movieDetails/MovieDetails.tsx
+++ movie-app/src/components/movieDetails/MovieDetails.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 import './MovieDetails.css'
 
 const MovieDetails = () => {
     return (
-        <div>MovieDetails</div>
+        <div className="movie-details-box">
+            <div>
+            <h1>Title</h1>
+            <button>play</button>
+            <h2>+</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nam ad laboriosam, quisquam voluptatibus ex incidunt aspernatur dolorum at in? Eum error alias voluptatibus odio obcaecati ab quasi fugit provident.</p>
+            </div>
+        </div>
     )
 }
 
