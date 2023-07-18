6b38122e7ff1554483f9602b980bbe944ede152b
neringa1991 <neringa1991@yahoo.com>
2023-07-18 18:42:40 +0300

    init: add the movies list components

---
+++ movie-app/src/components/watchedBox/WatchedBox.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {useState} from "react";
+import MoviesListComponent from "../moviesList/MoviesList";
+import MoviesSummary from "../moviesSummary/MoviesSummary";
+
+const WatchedBox = () => {
+  const tempMovieData = [
+    {
+      imdbID: "tt1375666",
+      Title: "Inception",
+      Year: "2010",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    },
+    {
+      imdbID: "tt0133093",
+      Title: "The Matrix",
+      Year: "1999",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    },
+    {
+      imdbID: "tt6751668",
+      Title: "Parasite",
+      Year: "2019",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    },
+  ];
+
+  const tempWatchedData: {
+    imdbID: string;
+    Title: string;
+    Year: string;
+    Poster: string;
+    runtime: number;
+    imdbRating: number;
+    userRating: number;
+  }[] = [
+    {
+      imdbID: "tt1375666",
+      Title: "Inception",
+      Year: "2010",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+      runtime: 148,
+      imdbRating: 8.8,
+      userRating: 10,
+    },
+    {
+      imdbID: "tt0088763",
+      Title: "Back to the Future",
+      Year: "1985",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+      runtime: 116,
+      imdbRating: 8.5,
+      userRating: 9,
+    },
+  ];
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  const avgImdbRating = tempWatchedData.map((movie) => movie.imdbRating);
+
+  const avgUserRating = tempWatchedData.map((movie) => movie.userRating);
+
+  const avgRuntime = tempWatchedData.map((movie) => movie.runtime);
+
+  return (
+    <div className="MovieComponentBox">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+
+      {isOpen1 && <MoviesSummary />}
+    </div>
+  );
+};
+
+export default WatchedBox;
