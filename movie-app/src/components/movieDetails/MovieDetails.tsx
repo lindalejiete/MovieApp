8582ab43e35081e093c0119a18125c6e8dee693d
neringa1991 <neringa1991@yahoo.com>
2023-07-20 15:56:08 +0300

    feat: add new component and movie details component fetching functionality

--- movie-app/src/components/movieDetails/MovieDetails.tsx
+++ movie-app/src/components/movieDetails/MovieDetails.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 import "./MovieDetails.css";
+import {useState, useEffect} from "react";
+const KEY = "9f8ff0fc";
 
 const MovieDetails = ({selectedId, onCloseMovie}: any) => {
+  const [movie, setMovie] = useState({});
+
+  useEffect(function () {
+    async function getMovieDetails() {
+      //      try {
+      //   setIsLoading(true);
+      //   setError("");
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+
+      // if (!res.ok) throw new Error("Something went wrong with fetching movie");
+      const data = await res.json();
+      //   if (data.Response === "False") throw new Error("Movie not found");
+
+      setMovie(data);
+      //   setMovies(data.Search);
+      // } catch (err: any) {
+      //   setError(err.message);
+      // } finally {
+      //   setIsLoading(false);
+      // }
+    }
+    // if (query.length === 0) {
+    //   setMovies(tempMovieData);
+    //   setError("");
+    //   return;
+    // }
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
       <button className="btn-back" onClick={onCloseMovie}>
--- movie-app/src/data/data.ts
+++ movie-app/src/data/data.ts
@@ -23,6 +23,75 @@ export const tempMovieData: searchedMoviesList[] = [
     Poster:
       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
   },
+  {
+    Title: "Mom and Dad",
+    Year: "2017",
+    imdbID: "tt5462326",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZGRkM2Q3MmEtNzIyZC00NDdmLWFiMjEtMjk2NjNlODYwOWVlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  },
+  {
+    Title:
+      "Marvel One-Shot: A Funny Thing Happened on the Way to Thor's Hammer",
+    Year: "2011",
+    imdbID: "tt2011109",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYmVlYTg3N2QtMWM2OS00YWQyLWI2M2MtMDc0ZjBkZjk1MTY3XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Funny Farm",
+    Year: "1988",
+    imdbID: "tt0095188",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BM2MyNmQ2ZTQtYzE1Ni00MWQwLWE3ZTctMDcxOWE4ZTQzMjE5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Cry Me a Sad River",
+    Year: "2018",
+    imdbID: "tt9036926",
+
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMzExZGZhZGQtNWI5YS00NjFiLTk4NTMtMTQyODJjODU2MDVlXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_SX300.jpg",
+  },
+
+  {
+    Title: "Honey, I Shrunk the Kids",
+    Year: "1989",
+    imdbID: "tt0097523",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTQ1NTg4MDAtOGU0OS00ZGQwLTliZjQtNDEzZjAzZGI5MjFjXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_SX300.jpg",
+  },
+  {
+    Title: "Cats & Dogs",
+    Year: "2001",
+    imdbID: "tt0239395",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BY2JmMDJlMmEtYTk4OS00YWQ5LTk2NzMtM2M3NzhkMjI4MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg",
+  },
+  {
+    Title: "Horror Express",
+    Year: "1972",
+    imdbID: "tt0068713",
+
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMmYzMjEwYzUtNjZlZC00OWUzLWJhNjMtZWIxMTQ2Njg0YTIyXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg",
+  },
+
+  {
+    Title: "Music and Lyrics",
+    Year: "2007",
+    imdbID: "tt0758766",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNDY5MTQwODI3OV5BMl5BanBnXkFtZTcwODM2MjEzMw@@._V1_SX300.jpg",
+  },
+
+  {
+    Title: "The Power of the Dog",
+    Year: "2021",
+    imdbID: "tt10293406",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZGRhYjE2NWUtN2FkNy00NGI3LTkxYWMtMDk4Yjg5ZjI3MWI2XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg",
+  },
 ];
 
 export const tempWatchedData: watchedMoviesList[] = [
