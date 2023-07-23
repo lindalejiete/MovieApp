b78a0cca31c5fefd7416a0e462378b97a93b8cc8
neringa1991 <neringa1991@yahoo.com>
2023-07-23 19:10:23 +0300

    feat: refactor code

--- movie-app/src/components/header/Header.tsx
+++ movie-app/src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import "./Header.css";
 import {HeaderComponentTypes} from "../../types/types";
 // @ts-ignore
 import Logo from "../../assets/logo.png";
+
 const Header = ({onSearch, query, movies}: HeaderComponentTypes) => {
   return (
     <div className="HeaderCenter">
